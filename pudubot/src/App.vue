20ca8960bdfe0c1bb7a74956d0c0e38d882e520a
FMIFH <48632339+FMIFH@users.noreply.github.com>
2020-11-03 12:53:34 +0000

    update

---
+++ pudubot/src/App.vue
@@ -0,0 +1,33 @@
+<template>
+  <img alt="Vue logo" src="./assets/logo2.png">
+  <HelloWorld msg="Welcome to Microsegur Pudubot App"/>
+</template>
+
+<script>
+import HelloWorld from './components/HelloWorld.vue'
+
+export default {
+  name: 'App',
+  components: {
+    HelloWorld
+  }
+}
+</script>
+
+<style>
+body {    
+    margin: 0 !important;
+    padding: 0 !important;
+}
+#app {
+  margin: 0;
+  padding: 0;
+  font-family: Georgia, 'Times New Roman', Times, serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: black;
+  background: lightgray;
+  height: 100vh;
+}
+</style>
